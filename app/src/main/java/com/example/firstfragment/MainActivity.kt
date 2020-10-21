07238287d14cb795665da8b118943ac2348bd762
Charity Howell <caccheme@hotmail.com>
2020-10-21 14:14:04 -0500

    make fragment persistent for the lifetime of the activity

--- app/src/main/java/com/example/firstfragment/MainActivity.kt
+++ app/src/main/java/com/example/firstfragment/MainActivity.kt
@@ -2,19 +2,19 @@ package com.example.firstfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.firstfragment.R.id.simple_fragment
-import kotlinx.android.synthetic.main.activity_main.*
-
+import android.view.View
 
 
 class MainActivity : AppCompatActivity() {
 
+    private val fragment: SimpleFragment = SimpleFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        val fragment = SimpleFragment()
-
+    fun clickHandler(@Suppress("UNUSED_PARAMETER")view: View) {
         supportFragmentManager
             .beginTransaction()
             .add(R.id.simple_fragment, fragment)
