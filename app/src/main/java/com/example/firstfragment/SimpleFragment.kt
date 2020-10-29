359aaf441eaaea66ca6970589f6afc1f8e7cdb80
Charity Howell <caccheme@hotmail.com>
2020-10-28 21:06:54 -0500

    Refactor, remove fragment 2 and clean up code

--- app/src/main/java/com/example/firstfragment/SimpleFragment.kt
+++ app/src/main/java/com/example/firstfragment/SimpleFragment.kt
@@ -1,26 +1,13 @@
 package com.example.firstfragment
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.example.firstfragment.constants.LIFECYCLE_TAG
 
 class SimpleFragment : Fragment() {
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        Log.i(LIFECYCLE_TAG, "onAttach")
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.i(LIFECYCLE_TAG, "onCreate")
-    }
 
     companion object {
         const val MESSAGE_KEY = "message_key"
@@ -40,7 +27,6 @@ class SimpleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        Log.i(LIFECYCLE_TAG, "onCreateView")
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_simple, container, false)
 
@@ -52,31 +38,6 @@ class SimpleFragment : Fragment() {
         }
 
         return view
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i(LIFECYCLE_TAG, "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i(LIFECYCLE_TAG, "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i(LIFECYCLE_TAG, "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i(LIFECYCLE_TAG, "onDestroy")
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        Log.i(LIFECYCLE_TAG, "onDetach")
     }
 
 }
