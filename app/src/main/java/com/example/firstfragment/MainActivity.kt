fe2cddc5bddf600fcf50b0711d2f41a58959ccb2
Charity Howell <caccheme@hotmail.com>
2020-10-31 04:53:10 -0500

    Detect screen size with resource selectors, wide -> side by side view

--- app/src/main/java/com/example/firstfragment/MainActivity.kt
+++ app/src/main/java/com/example/firstfragment/MainActivity.kt
@@ -1,18 +1,31 @@
 package com.example.firstfragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.example.firstfragment.constants.FRAGMENT_TAG
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        this.setSupportActionBar(toolbar)
+
+        val fragmentContainer = findViewById<ViewGroup>(R.id.detail_fragment_container)
+        val mTablet: Boolean = (fragmentContainer != null)
+
+        val tvOut: TextView = findViewById(R.id.textOut)
+        tvOut.text = "Fragments side-by-side? $mTablet"
     }
 
     fun addClickHandler(@Suppress("UNUSED_PARAMETER") view: View) {
