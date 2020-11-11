fa63e6e649a86c38fb911189fb3f5d79b4909042
Charity Howell <caccheme@hotmail.com>
2020-11-11 06:19:48 -0600

    Wrap an alert dialog in a fragment, refactor to remove code from main activity

--- app/src/main/java/com/example/firstfragment/MainActivity.kt
+++ app/src/main/java/com/example/firstfragment/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.example.firstfragment
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity(), DetailFragment.FragmentListener {
+class MainActivity : AppCompatActivity() {
 
-    private val myFragmentFinishTAG: String = "MainActivity"
     private var mTablet: Boolean = true
 
     @SuppressLint("SetTextI18n")
@@ -26,40 +23,13 @@ class MainActivity : AppCompatActivity(), DetailFragment.FragmentListener {
         tvOut.text = "Fragments side-by-side? $mTablet"
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { viewDetailFragment() }
+
+        fab.setOnClickListener { showDialog() }
     }
 
-    private fun viewDetailFragment() {
-
-        val person = Person("Mickey", "Mouse", 35)
-
-        if (mTablet) {
-            val fragmentManager = supportFragmentManager
-            val fragment = DetailFragment.newInstance(person)
-            fragmentManager
-                .beginTransaction()
-                .addToBackStack(null)
-                .add(R.id.detail_fragment_container, fragment)
-                .commit()
-        } else {
-            val intent = Intent(this, DetailActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    override fun onFragmentFinish(person: Person) {
-        Log.i(
-            myFragmentFinishTAG, "onFragmentFinish: " +
-                    "${person.firstName}, ${person.lastName}, ${person.age}"
-        )
-
-        val fragment = supportFragmentManager
-            .findFragmentById(R.id.detail_fragment_container) as DetailFragment
-
-        supportFragmentManager
-            .beginTransaction()
-            .remove(fragment)
-            .commit()
+    private fun showDialog() {
+        val dialogFragment = AlertDialogFragment()
+        dialogFragment.show(supportFragmentManager, "DIALOG_FRAGMENT")
     }
 
 }
