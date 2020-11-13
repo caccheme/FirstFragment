f7bcdb8606a8520e69a302736346d126a1ec028c
Charity Howell <caccheme@hotmail.com>
2020-11-13 06:33:12 -0600

    pass arguments to dialog

--- app/src/main/java/com/example/firstfragment/DataEntryDialog.kt
+++ app/src/main/java/com/example/firstfragment/DataEntryDialog.kt
@@ -4,15 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.DialogFragment
+import com.example.firstfragment.constants.PERSON_KEY
 
 class DataEntryDialog : DialogFragment() {
+    var etFirstName: EditText? = null
+    var etLastName:EditText? = null
+    var etAge:EditText? = null
+
+    fun newInstance(person: Person): DataEntryDialog {
+        val args = Bundle()
+        args.putParcelable(PERSON_KEY, person)
+        val fragment = DataEntryDialog()
+        fragment.arguments = args
+        return fragment
+    }
+    
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.data_entry_dialog, container, false)
+
+        etFirstName = rootView.findViewById<View>(R.id.textFirstName) as EditText
+        etLastName = rootView.findViewById<View>(R.id.textLastName) as EditText
+        etAge = rootView.findViewById<View>(R.id.textAge) as EditText
+
+        val person = this.arguments?.getParcelable<Person>(PERSON_KEY)
+        etFirstName!!.setText(person?.firstName)
+        etLastName!!.setText(person?.lastName)
+        etAge!!.setText(person?.age.toString())
 
         val btnOk = rootView.findViewById<View>(R.id.btnOk)
         btnOk.setOnClickListener { dismiss() }
--- app/src/main/java/com/example/firstfragment/constants/Constants.kt
+++ app/src/main/java/com/example/firstfragment/constants/Constants.kt
@@ -1,3 +1,4 @@
 package com.example.firstfragment.constants
 
 const val FRAGMENT_TAG = "1234"
+const val PERSON_KEY = "person"
