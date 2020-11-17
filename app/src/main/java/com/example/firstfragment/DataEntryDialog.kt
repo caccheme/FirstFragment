e32f2bad62afba71aa5614268d71886420a8ec4e
Charity Howell <caccheme@hotmail.com>
2020-11-17 06:31:58 -0600

    use callback methods in dialog fragment

--- app/src/main/java/com/example/firstfragment/DataEntryDialog.kt
+++ app/src/main/java/com/example/firstfragment/DataEntryDialog.kt
@@ -1,17 +1,21 @@
 package com.example.firstfragment
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import androidx.fragment.app.DialogFragment
+import com.example.firstfragment.constants.FRAGMENT_TAG
 import com.example.firstfragment.constants.PERSON_KEY
 
 class DataEntryDialog : DialogFragment() {
     var etFirstName: EditText? = null
     var etLastName:EditText? = null
     var etAge:EditText? = null
+    private lateinit var mListener: DataEntryListener
 
     fun newInstance(person: Person): DataEntryDialog {
         val args = Bundle()
@@ -19,6 +23,14 @@ class DataEntryDialog : DialogFragment() {
         val fragment = DataEntryDialog()
         fragment.arguments = args
         return fragment
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context is DataEntryListener) {
+            mListener = context
+        }
     }
     
     override fun onCreateView(
@@ -38,11 +50,28 @@ class DataEntryDialog : DialogFragment() {
         etAge!!.setText(person?.age.toString())
 
         val btnOk = rootView.findViewById<View>(R.id.btnOk)
-        btnOk.setOnClickListener { dismiss() }
+        btnOk.setOnClickListener {
+            saveData() }
 
         val btnCancel = rootView.findViewById<View>(R.id.btnCancel)
         btnCancel.setOnClickListener { dismiss() }
 
         return rootView
+    }
+
+    private fun saveData() {
+        val person = Person("", "", 0)
+        person.firstName = etFirstName?.text.toString()
+        person.lastName = etLastName?.text.toString()
+        person.age = Integer.valueOf(etAge?.text.toString())
+
+        mListener.onDataEntryComplete(person)
+        dismiss()
+    }
+
+    interface DataEntryListener {
+        fun onDataEntryComplete(person: Person) {
+            Log.i(FRAGMENT_TAG, "onDataEntryDialogFinish: ${person.firstName}, ${person.lastName}, ${person.age}")
+        }
     }
 }
--- app/src/main/java/com/example/firstfragment/MainActivity.kt
+++ app/src/main/java/com/example/firstfragment/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.firstfragment
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    DataEntryDialog.DataEntryListener {
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +25,11 @@ class MainActivity : AppCompatActivity() {
 
         val dialog = DataEntryDialog().newInstance(p)
         dialog.show(supportFragmentManager, "DIALOG_FRAGMENT")
+    }
+
+    override fun onDataEntryComplete(person: Person){
+        Toast.makeText(this, "You entered ${person.firstName} ${person.lastName}",
+            Toast.LENGTH_SHORT).show()
     }
 
 }
