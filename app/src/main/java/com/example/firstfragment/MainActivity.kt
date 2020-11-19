d6cdffa132d4d456c237f89e00b9ae222cf59fe5
Charity Howell <caccheme@hotmail.com>
2020-11-19 06:31:40 -0600

    create ViewPager adapter

--- app/src/main/java/com/example/firstfragment/MainActivity.kt
+++ app/src/main/java/com/example/firstfragment/MainActivity.kt
@@ -2,13 +2,36 @@ package com.example.firstfragment
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.example.david.viewpager.DataProvider
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
-    private val products: List<Product> = DataProvider.productList
-    private val numPages = products.size
+    private lateinit var mPager: ViewPager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mPager = findViewById(R.id.pager)
+        val pagerAdapter = ViewPagerAdapter(supportFragmentManager)
+        mPager.adapter = pagerAdapter
+    }
+
+    private class ViewPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+        private val products: List<Product> = DataProvider.productList
+        private val numPages = products.size
+
+        override fun getCount(): Int {
+            return numPages
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return ItemFragment.newInstance(products[position])
+        }
+
     }
 }
